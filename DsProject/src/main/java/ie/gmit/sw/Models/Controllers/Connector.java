2251eafcc3ae270a637d63d3daf39f0551390320
cwoodlock <g00341460@gmit.ie>
2018-12-10 21:37:06 +0000

    working on building the RMI server

---
+++ DsProject/src/main/java/ie/gmit/sw/Models/Controllers/Connector.java
@@ -0,0 +1,28 @@
+package ie.gmit.sw.Models.Controllers;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.List;
+import ie.gmit.sw.Models.RMI.Booking;
+
+public class Connector {
+
+	private RmiInterface bookingService;
+
+	public Connector() {
+		System.out.println("Entered Connector");
+
+		try {
+			this.bookingService = (RmiInterface) Naming.lookup("rmi://localhost:1099/carBookingService");
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("leaving Connector");
+
+	}
+
+}
+
---
+++ DsProject/src/main/java/ie/gmit/sw/Models/Controllers/RmiInterface.java
@@ -0,0 +1,20 @@
+package ie.gmit.sw.Models.Controllers;
+
+import java.rmi.*;
+import java.util.List;
+
+import ie.gmit.sw.Models.RMI.Booking;
+
+public interface RmiInterface extends Remote{
+	//Create a car hire
+	public void createCarHire(String query) throws RemoteException; 
+	
+	//Put all the bookings in a list
+	public List<Booking> readCarHire() throws RemoteException; 
+	
+	//Update a car hire
+	public void updateCarHire(String query) throws RemoteException; 
+	
+	//Delete a car hire
+	public void deleteCarHire(String query) throws RemoteException; 
+}
